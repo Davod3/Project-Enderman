757dd568ce98b6f419980821897bed919e51072e
David Pereira <62812324+Davod3@users.noreply.github.com>
2023-07-13 11:46:45 +0100

    Created project backend

---
+++ Backend/src/main/java/com/project/enderman/EndermanApplication.java
@@ -0,0 +1,13 @@
+package com.project.enderman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndermanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndermanApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/project/enderman/EndermanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.enderman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndermanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
