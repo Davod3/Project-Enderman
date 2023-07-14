46e78d86a46524ac0f8fe4e2a1b271ab79591b5d
David Pereira <62812324+Davod3@users.noreply.github.com>
2023-07-14 15:38:52 +0100

    Created CreateServerHandler skeleton

---
+++ Backend/src/main/java/com/project/enderman/handlers/CreateServerHandler.java
@@ -0,0 +1,21 @@
+package com.project.enderman.handlers;
+
+public class CreateServerHandler {
+
+    public boolean createServer(String name, String port){
+
+        return false;
+    }
+    public boolean downloadServer(String url, String serverID){
+
+        //https://www.curseforge.com/api/v1/mods/715572/files/4642332/download
+
+        return false;
+    }
+
+    public boolean selectStartScript(String filePath, String serverID) {
+
+        return false;
+    }
+
+}
