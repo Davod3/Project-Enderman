03d3f91166467a14163d2e6a079dee2b7c04ff2b
David Pereira <62812324+Davod3@users.noreply.github.com>
2023-07-18 14:08:29 +0100

    Refactored utils to separate download / unzip functionalities

--- Backend/src/main/java/com/project/enderman/utils/Compression.java
+++ Backend/src/main/java/com/project/enderman/utils/Compression.java
@@ -2,8 +2,10 @@ package com.project.enderman.utils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class Compression {
@@ -49,5 +51,55 @@ public class Compression {
         }
 
         fis.close();
+    }
+
+    public static void unzip(File destDir, ZipInputStream zis) throws IOException {
+
+        byte[] buffer = new byte[1024];
+        ZipEntry zipEntry = zis.getNextEntry();
+
+        while (zipEntry != null) {
+
+            System.out.println("Unzipping: " + zipEntry.getName());
+
+            File newFile = newFile(destDir, zipEntry);
+            if (zipEntry.isDirectory()) {
+                if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                    throw new IOException("Failed to create directory" + newFile);
+                }
+            } else {
+                // fix for Windows files
+                File parent = newFile.getParentFile();
+                if (!parent.isDirectory() && !parent.mkdirs()) {
+                    throw new IOException("Failed to create directory" + parent);
+                }
+
+                // write file
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+            }
+            zipEntry = zis.getNextEntry();
+        }
+
+        zis.closeEntry();
+    }
+
+    private static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
     }
 }
--- Backend/src/main/java/com/project/enderman/utils/Downloader.java
+++ Backend/src/main/java/com/project/enderman/utils/Downloader.java
@@ -1,11 +1,7 @@
 package com.project.enderman.utils;
 
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
-import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class Downloader {
@@ -18,38 +14,10 @@ public class Downloader {
             BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
 
             File destDir = new File(destPath);
-
-            byte[] buffer = new byte[1024];
             ZipInputStream zis = new ZipInputStream(in);
-            ZipEntry zipEntry = zis.getNextEntry();
 
-            while (zipEntry != null) {
-
-                System.out.println("Unzipping: " + zipEntry.getName());
-
-                File newFile = newFile(destDir, zipEntry);
-                if (zipEntry.isDirectory()) {
-                    if (!newFile.isDirectory() && !newFile.mkdirs()) {
-                        throw new IOException("Failed to create directory" + newFile);
-                    }
-                } else {
-                    // fix for Windows files
-                    File parent = newFile.getParentFile();
-                    if (!parent.isDirectory() && !parent.mkdirs()) {
-                        throw new IOException("Failed to create directory" + parent);
-                    }
-
-                    // write file
-                    FileOutputStream fos = new FileOutputStream(newFile);
-                    int len;
-
-                    while ((len = zis.read(buffer)) > 0) {
-                        fos.write(buffer, 0, len);
-                    }
-                    fos.close();
-                }
-                zipEntry = zis.getNextEntry();
-            }
+            Compression.unzip(destDir, zis);
+            zis.close();
 
             return true;
 
@@ -60,19 +28,6 @@ public class Downloader {
 
     }
 
-    private static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
-
-        File destFile = new File(destinationDir, zipEntry.getName());
-
-        String destDirPath = destinationDir.getCanonicalPath();
-        String destFilePath = destFile.getCanonicalPath();
-
-        if (!destFilePath.startsWith(destDirPath + File.separator)) {
-            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
-        }
-
-        return destFile;
-    }
 
 }
 
