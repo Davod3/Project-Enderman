11765d55e7bf06d1c6d7e886a173dbeb53e8be14
David Pereira <62812324+Davod3@users.noreply.github.com>
2023-07-24 15:32:24 +0100

    Added endpoint to check api credentials on backend

---
+++ Backend/src/main/java/com/project/enderman/controllers/LoginController.java
@@ -0,0 +1,18 @@
+package com.project.enderman.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("api")
+public class LoginController {
+
+    @PostMapping("/test_token")
+    public void testToken(){
+        /*
+            Empty on purpose. This endpoint only exists so that clients can check
+            if the provided api token works
+         */
+    }
+}
