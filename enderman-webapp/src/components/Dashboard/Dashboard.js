57802032b47ed479670a19bb65ed02d38045c27c
Davod3 <davidpereira.pereira4@gmail.com>
2023-07-25 18:15:03 +0100

    Created dashboard page

--- enderman-webapp/src/components/Dashboard/Dashboard.js
+++ enderman-webapp/src/components/Dashboard/Dashboard.js
@@ -1,7 +1,22 @@
 import React from "react";
+import * as Icon from 'react-bootstrap-icons';
+import './Dashboard.css'
 
 export default function Dashboard() {
     return(
-        <h2>Dashboard</h2>
+        
+        <div className="dashboard-wrapper">
+
+            <h1 id='dashboard-title'>Dashboard</h1>
+
+            <div id='list-wrapper'>
+               
+                <Icon.PlusCircle size={30}></Icon.PlusCircle>
+                <hr/>
+
+            </div>
+
+        </div>
+
     );
 }
